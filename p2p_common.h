14a3f08427751b52cf3e8ccdc2111df3de912f25
Nosov Vladimir <hostage222@gmail.com>
2017-06-12 19:57:41 +0300

    проект создан

---
+++ p2p_common.h
@@ -0,0 +1,18 @@
+#ifndef P2P_COMMON_H
+#define P2P_COMMON_H
+
+#include <cstdint>
+#include <string>
+
+namespace p2p
+{
+	
+/**
+ * @brief Глобально уникальный идентификатор пользователя (контакта);
+ * формат строго не определён
+ */
+using friend_id_type = std::string;
+
+}
+
+#endif // P2P_COMMON_H
