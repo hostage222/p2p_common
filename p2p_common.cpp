9349fb1c258a02111bcf0d6e442546148d4c6fb2
Nosov Vladimir <hostage222@gmail.com>
2017-07-01 20:36:40 +0300

    добавлены методы для работы с сообщениями

--- p2p_common.cpp
+++ p2p_common.cpp
@@ -1,2 +1,373 @@
 #include "p2p_common.h"
 
+#include <cstdint>
+#include <string>
+#include <array>
+#include <utility>
+#include <boost/asio.hpp>
+
+using namespace std;
+
+namespace p2p
+{
+
+size_t analyze_buf(const buffer_type &buf, size_t bytes, bool *valid = 0);
+bool has_special_symbols(const string &param);
+bool append_symbol(buffer_type &buf, size_t &bytes, char symbol);
+
+string to_string(const boost::asio::ip::tcp::endpoint &endpoint)
+{
+    return endpoint.address().to_string() + ":" +
+            std::to_string(endpoint.port());
+}
+
+size_t read_complete(const buffer_type &buf, size_t bytes)
+{
+    return analyze_buf(buf, bytes);
+}
+
+bool is_valid_message(const buffer_type &buf, size_t bytes)
+{
+    bool res;
+    analyze_buf(buf, bytes, &res);
+    return res;
+}
+
+buf_sequence get_buf_sequence(buffer_type &buf, size_t bytes)
+{
+    if (bytes == 0)
+    {
+        return buf_sequence{buf.begin(), buf.begin()};
+    }
+
+    if (buf[bytes - 1] == '\n')
+    {
+        return buf_sequence{buf.begin(), buf.begin() + bytes - 1};
+    }
+    else
+    {
+        return buf_sequence{buf.begin(), buf.begin() + bytes};
+    }
+}
+
+bool is_empty(buf_sequence buf)
+{
+    return buf.first == buf.second;
+}
+
+buf_sequence read_token(buf_sequence &buf)
+{
+    if (buf.first == buf.second)
+    {
+        throw invalid_token_exception{};
+    }
+
+    buf_sequence res;
+    if (*buf.first == '"')
+    {
+        ++buf.first;
+        res.first = buf.first;
+
+        bool spec_symbol = false;
+        while (buf.first != buf.second)
+        {
+            char c = *buf.first;
+            if (spec_symbol)
+            {
+                if (c == '\\' || c == '*')
+                {
+                    spec_symbol = false;
+                }
+                else
+                {
+                    throw invalid_token_exception{};
+                }
+            }
+            else
+            {
+                if (c == '"')
+                {
+                    break;
+                }
+                else if (c == '\\')
+                {
+                    spec_symbol = true;
+                }
+            }
+            ++buf.first;
+        }
+
+        if (buf.first != buf.second)
+        {
+            res.second = buf.first;
+            if (res.first == res.second)
+            {
+                throw invalid_token_exception{};
+            }
+            ++buf.first;
+            if (buf.first != buf.second)
+            {
+                if (*buf.first == ' ')
+                {
+                    ++buf.first;
+                }
+                else
+                {
+                    throw invalid_token_exception{};
+                }
+            }
+        }
+        else
+        {
+            throw invalid_token_exception{};
+        }
+    }
+    else
+    {
+        auto it = find(buf.first, buf.second, ' ');
+        res.first = buf.first;
+        res.second = it;
+        if (it != buf.second)
+        {
+            buf.first = next(it);
+        }
+        else
+        {
+            buf.first = it;
+        }
+    }
+    return res;
+}
+
+string token_to_string(buf_sequence buf)
+{
+    if (buf.first == buf.second)
+    {
+        throw invalid_token_exception{};
+    }
+
+    if (*buf.first == '"')
+    {
+        string res;
+        bool spec_symbol = false;
+        while (buf.first != buf.second)
+        {
+            char c = *buf.first;
+            if (spec_symbol)
+            {
+                if (c == '\\' || c == '*')
+                {
+                    res.push_back(c);
+                    spec_symbol = false;
+                }
+                else
+                {
+                    throw invalid_token_exception{};
+                }
+            }
+            else
+            {
+                if (c == '"')
+                {
+                    break;
+                }
+                else if (c == '\\')
+                {
+                    spec_symbol = true;
+                }
+                else
+                {
+                    res.push_back(c);
+                }
+            }
+            ++buf.first;
+        }
+
+        if (buf.first != buf.second && next(buf.first) == buf.second)
+        {
+            return res;
+        }
+        else
+        {
+            throw invalid_token_exception{};
+        }
+    }
+    else
+    {
+        auto it = find(buf.first, buf.second, ' ');
+        if (it == buf.second)
+        {
+            return string{buf.first, buf.second};
+        }
+        else
+        {
+            throw invalid_token_exception{};
+        }
+    }
+}
+
+string read_string(buf_sequence &buf)
+{
+    return token_to_string(read_token(buf));
+}
+
+bool write_command(buffer_type &buf, size_t &bytes, const string &cmd)
+{
+    if (cmd.size() > buf.size())
+    {
+        return false;
+    }
+
+    bytes = cmd.size();
+    copy(cmd.begin(), cmd.end(), buf.begin());
+    return true;
+}
+
+bool append_param(buffer_type &buf, size_t &bytes, const string &param)
+{
+    size_t new_size = bytes;
+    if (!append_symbol(buf, new_size, ' '))
+    {
+        return false;
+    }
+
+    if (has_special_symbols(param))
+    {
+        if (!append_symbol(buf, new_size, '"'))
+        {
+            return false;
+        }
+
+        for (char c : param)
+        {
+            if (c == '\\')
+            {
+                if (!append_symbol(buf, new_size, '\\') ||
+                    !append_symbol(buf, new_size, '\\'))
+                {
+                    return false;
+                }
+            }
+            else if (c == '*')
+            {
+                if (!append_symbol(buf, new_size, '*') ||
+                    !append_symbol(buf, new_size, '*'))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (!append_symbol(buf, new_size, c))
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (!append_symbol(buf, new_size, '"'))
+        {
+            return false;
+        }
+    }
+    else
+    {
+        if (new_size + param.size() > buf.size())
+        {
+            return false;
+        }
+
+        copy(param.begin(), param.end(), buf.begin() + new_size);
+        new_size += param.size();
+    }
+
+    bytes = new_size;
+    return true;
+}
+
+size_t analyze_buf(const buffer_type &buf, size_t bytes, bool *valid)
+{
+    bool spec_symbol = false;
+    bool raw_input = false;
+    bool v = true;
+    size_t res = 1;
+
+    if (bytes == 0)
+    {
+        return 1;
+    }
+
+    for (size_t i = 0; i < bytes; ++i)
+    {
+        uint8_t d = buf[i];
+        if (spec_symbol)
+        {
+            if (d != '\\' && d != '*')
+            {
+                v = false;
+                res = 0;
+                break;
+            }
+            spec_symbol = false;
+        }
+        else if (raw_input)
+        {
+            if (d == '\\')
+            {
+                spec_symbol = true;
+            }
+            else if (d == '*')
+            {
+                raw_input = false;
+            }
+        }
+        else
+        {
+            if (d == '*')
+            {
+                raw_input = true;
+            }
+        }
+    }
+
+    if (buf[bytes - 1] == '\n')
+    {
+        res = 0;
+    }
+    else if (bytes >= MAX_BUFFER_SIZE)
+    {
+        v = false;
+        res = 0;
+    }
+
+    if (valid)
+    {
+        *valid = v;
+    }
+    return res;
+}
+
+bool has_special_symbols(const string &param)
+{
+    return param.find_first_of(" \n*") != string::npos;
+}
+
+bool append_symbol(buffer_type &buf, size_t &bytes, char symbol)
+{
+    if (bytes >= buf.size())
+    {
+        return false;
+    }
+
+    buf[bytes++] = symbol;
+    return true;
+}
+
+string to_string(version_type version)
+{
+    return std::to_string(version.major) + "." +
+            std::to_string(version.minor) + "." +
+            std::to_string(version.patch);
+}
+
+}//p2p
--- p2p_common.h
+++ p2p_common.h
@@ -3,15 +3,56 @@
 
 #include <cstdint>
 #include <string>
+#include <array>
+#include <utility>
+#include <boost/asio.hpp>
 
 namespace p2p
 {
-	
+
+struct version_type { int major; int minor; int patch; };
+
 /**
  * @brief Глобально уникальный идентификатор пользователя (контакта);
  * формат строго не определён
  */
 using friend_id_type = std::string;
+
+std::string to_string(const boost::asio::ip::tcp::endpoint &endpoint);
+std::string to_string(version_type version);
+
+/*
+ * Формат сообщения: "команда [данные]\n" (без кавычек);
+ * данные могут содержать любое количество параметров, разделённых пробелом;
+ * если внутри параметра находятся символы пробела или конца
+ * строки, этот параметр выделяется кавычками, кавычки внутри
+ * параметра представляются как \", обратная черта как \\
+ *
+ */
+constexpr size_t MAX_BUFFER_SIZE = 10000;
+using buffer_type = std::array<uint8_t, MAX_BUFFER_SIZE>;
+using buf_iterator = buffer_type::iterator;
+using buf_sequence = std::pair<buf_iterator, buf_iterator>;
+
+size_t read_complete(const buffer_type &buf, size_t bytes);
+bool is_valid_message(const buffer_type &buf, size_t bytes);
+
+buf_sequence get_buf_sequence(buffer_type &buf, size_t bytes);
+bool is_empty(buf_sequence buf);
+
+struct invalid_token_exception {};
+buf_sequence read_token(buf_sequence &buf);
+std::string token_to_string(buf_sequence buf);
+std::string read_string(buf_sequence &buf);
+
+bool write_command(buffer_type &buf, size_t &bytes, const std::string &cmd);
+bool append_param(buffer_type &buf, size_t &bytes, const std::string &param);
+
+const std::string GET_VERSION = "GET_VERSION";
+
+const std::string INVALID_FORMAT = "INVALID_FORMAT";
+const std::string INVALID_COMMAND = "INVALID_COMMAND";
+const std::string INVALID_DATA = "INVALID_DATA";
 
 }
 
